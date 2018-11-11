1da21c924cae99210390628f943b24a984f606bb
casainho <casainho@gmail.com>
2018-11-11 20:01:52 +0000

    Added 128MHz system clock configuration

--- Bafang_LCD_850C/Bafang_LCD_850C_firmware/main.c
+++ Bafang_LCD_850C/Bafang_LCD_850C_firmware/main.c
@@ -19,6 +19,8 @@
 #include "ugui/ugui.h"
 #include "ugui_driver/ugui_bafang_500c.h"
 
+void SetSysClockTo128Mhz(void);
+
 void system_power (uint32_t ui32_state)
 {
   if (ui32_state)
@@ -34,6 +36,8 @@ void system_power (uint32_t ui32_state)
 int main(void)
 {
   extern uint8_t BigFont[];
+
+  SetSysClockTo128Mhz();
 
   RCC_APB1PeriphResetCmd(RCC_APB1Periph_WWDG, DISABLE);
 
@@ -55,7 +59,15 @@ int main(void)
   while (1) {
   /* Clear screen. */
 //  UTFT_clrScr();
-  UG_DrawFrame(100, 100, 10, 10, C_PINK);     // Draw a Frame
+//  UG_DrawFrame(100, 100, 10, 10, C_PINK);     // Draw a Frame
+
+//  UG_DrawCircle( UG_S16 x0, UG_S16 y0, UG_S16 r, UG_COLOR c );
+  UG_FillCircle(100, 100, 50, C_MAGENTA);
+
+  while (1) ;
+
+
+
 //  UG_FillScreen(VGA_YELLOW);
 
 //      UG_FontSelect(&FONT_8X14);
@@ -89,5 +101,66 @@ int main(void)
   while (1)
   {
 
+  }
+}
+
+void SetSysClockTo128Mhz(void)
+{
+  ErrorStatus HSEStartUpStatus;
+
+  /* SYSCLK, HCLK, PCLK2 and PCLK1 configuration -----------------------------*/
+  /* RCC system reset(for debug purpose) */
+  RCC_DeInit();
+
+  /* Enable HSE */
+  RCC_HSEConfig(RCC_HSE_ON);
+
+  /* Wait till HSE is ready */
+  HSEStartUpStatus = RCC_WaitForHSEStartUp();
+
+  if (HSEStartUpStatus == SUCCESS)
+  {
+    /* Enable Prefetch Buffer */
+    FLASH_PrefetchBufferCmd(FLASH_PrefetchBuffer_Enable);
+
+    /* Flash 2 wait state */
+    FLASH_SetLatency(FLASH_Latency_2);
+
+    /* HCLK = SYSCLK */
+    RCC_HCLKConfig(RCC_SYSCLK_Div1);
+
+    /* PCLK2 = HCLK */
+    RCC_PCLK2Config(RCC_HCLK_Div1);
+
+    /* PCLK1 = HCLK/2 */
+    RCC_PCLK1Config(RCC_HCLK_Div2);
+
+    /* PLLCLK = 8MHz * 9 = 72 MHz */
+    RCC_PLLConfig(RCC_PLLSource_HSE_Div1, RCC_PLLMul_16);
+
+    /* Enable PLL */
+    RCC_PLLCmd(ENABLE);
+
+    /* Wait till PLL is ready */
+    while (RCC_GetFlagStatus(RCC_FLAG_PLLRDY) == RESET)
+    {
+    }
+
+    /* Select PLL as system clock source */
+    RCC_SYSCLKConfig(RCC_SYSCLKSource_PLLCLK);
+
+    /* Wait till PLL is used as system clock source */
+    while(RCC_GetSYSCLKSource() != 0x08)
+    {
+    }
+  }
+  else
+  { /* If HSE fails to start-up, the application will have wrong clock configuration.
+       User can add here some code to deal with this error */
+
+    /* Go to infinite loop */
+    while (1)
+    {
+    }
   }
 }

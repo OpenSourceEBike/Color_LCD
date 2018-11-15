1080eb74179db1d648e53a4760cb34b1c7add7ef
casainho <casainho@gmail.com>
2018-11-15 10:02:42 +0000

    Fix for fill screen.

--- Bafang_LCD_850C/Bafang_LCD_850C_firmware/ugui/ugui.c
+++ Bafang_LCD_850C/Bafang_LCD_850C_firmware/ugui/ugui.c
@@ -4346,7 +4346,8 @@ void UG_FontSelect( const UG_FONT* font )
 
 void UG_FillScreen( UG_COLOR c )
 {
-   UG_FillFrame(0,0,gui->x_dim-1,gui->y_dim-1,c);
+//   UG_FillFrame(0, 0, gui->x_dim - 1, gui->y_dim - 1, c);
+   UG_FillFrame(0, 0, gui->x_dim + 1, gui->y_dim - 1, c); // for some reason, just like this the full LCD get´s filled
 }
 
 void UG_FillFrame( UG_S16 x1, UG_S16 y1, UG_S16 x2, UG_S16 y2, UG_COLOR c )

0542c97c5affe354d57c615b048bf881e49e9551
casainho <casainho@gmail.com>
2019-04-07 03:01:54 +0100

    Motor works

--- Bafang_color_LCD_850C/Bafang_LCD_850C_firmware/src/main.c
+++ Bafang_color_LCD_850C/Bafang_LCD_850C_firmware/src/main.c
@@ -53,13 +53,7 @@ int main(void)
   // block until user release the buttons
   while (buttons_get_onoff_state() ||
       buttons_get_down_state() ||
-      buttons_get_up_state()) ;
-
-  GPIO_InitTypeDef GPIO_InitStructure;
-  GPIO_InitStructure.GPIO_Pin = USART1_TX__PIN;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-  GPIO_Init(USART1__PORT, &GPIO_InitStructure);
+      buttons_get_up_state());
 
   while(1)
   {

91894701479f75be118f0b2b10cda0d21c2aa142
casainho <casainho@gmail.com>
2019-07-18 14:16:47 +0100

    EEPROM improvement

--- Bafang_color_LCD_850C/Bafang_LCD_850C_firmware/src/eeprom.h
+++ Bafang_color_LCD_850C/Bafang_LCD_850C_firmware/src/eeprom.h
@@ -10,86 +10,56 @@
 #define _EEPROM_H_
 
 #include "lcd.h"
+#include "lcd_configurations.h"
+#include "main.h"
 
-#define KEY                                                                 0x01
+#define ADDRESS_KEY 0
+#define KEY 1
 
-#define ADDRESS_KEY                                                         0
-#define ADDRESS_ASSIST_LEVEL                                                1
-#define ADDRESS_WHEEL_PERIMETER_0                                           2
-#define ADDRESS_WHEEL_PERIMETER_1                                           3
-#define ADDRESS_MAX_WHEEL_SPEED                                             4
-#define ADDRESS_UNITS_TYPE                                                  5
-#define ADDRESS_HW_X10_OFFSET_0                                             6
-#define ADDRESS_HW_X10_OFFSET_1                                             7
-#define ADDRESS_HW_X10_OFFSET_2                                             8
-#define ADDRESS_HW_X10_OFFSET_3                                             9
-#define ADDRESS_HW_X10_100_PERCENT_OFFSET_0                                 10
-#define ADDRESS_HW_X10_100_PERCENT_OFFSET_1                                 11
-#define ADDRESS_HW_X10_100_PERCENT_OFFSET_2                                 12
-#define ADDRESS_HW_X10_100_PERCENT_OFFSET_3                                 13
-#define ADDRESS_SHOW_NUMERIC_BATTERY_SOC                                    14
-#define ADDRESS_ODOMETER_FIELD_STATE                                        15
-#define ADDRESS_BATTERY_MAX_CURRENT                                         16
-#define ADDRESS_TARGET_MAX_BATTERY_POWER                                    17
-#define ADDRESS_BATTERY_CELLS_NUMBER                                        18
-#define ADDRESS_BATTERY_LOW_VOLTAGE_CUT_OFF_X10_0                           19
-#define ADDRESS_BATTERY_LOW_VOLTAGE_CUT_OFF_X10_1                           20
-#define ADDRESS_CONFIG_0                                                    21
-#define ADDRESS_ASSIST_LEVEL_FACTOR_1                                       22
-#define ADDRESS_ASSIST_LEVEL_FACTOR_2                                       23
-#define ADDRESS_ASSIST_LEVEL_FACTOR_3                                       24
-#define ADDRESS_ASSIST_LEVEL_FACTOR_4                                       25
-#define ADDRESS_ASSIST_LEVEL_FACTOR_5                                       26
-#define ADDRESS_ASSIST_LEVEL_FACTOR_6                                       27
-#define ADDRESS_ASSIST_LEVEL_FACTOR_7                                       28
-#define ADDRESS_ASSIST_LEVEL_FACTOR_8                                       29
-#define ADDRESS_ASSIST_LEVEL_FACTOR_9                                       30
-#define ADDRESS_NUMBER_OF_ASSIST_LEVELS                                     31
-#define ADDRESS_STARTUP_MOTOR_POWER_BOOST_FEATURE_ENABLED                   32
-#define ADDRESS_STARTUP_MOTOR_POWER_BOOST_STATE                             33
-#define ADDRESS_STARTUP_MOTOR_POWER_BOOST_ASSIST_LEVEL_1                    34
-#define ADDRESS_STARTUP_MOTOR_POWER_BOOST_ASSIST_LEVEL_2                    35
-#define ADDRESS_STARTUP_MOTOR_POWER_BOOST_ASSIST_LEVEL_3                    36
-#define ADDRESS_STARTUP_MOTOR_POWER_BOOST_ASSIST_LEVEL_4                    37
-#define ADDRESS_STARTUP_MOTOR_POWER_BOOST_ASSIST_LEVEL_5                    38
-#define ADDRESS_STARTUP_MOTOR_POWER_BOOST_ASSIST_LEVEL_6                    39
-#define ADDRESS_STARTUP_MOTOR_POWER_BOOST_ASSIST_LEVEL_7                    40
-#define ADDRESS_STARTUP_MOTOR_POWER_BOOST_ASSIST_LEVEL_8                    41
-#define ADDRESS_STARTUP_MOTOR_POWER_BOOST_ASSIST_LEVEL_9                    42
-#define ADDRESS_STARTUP_MOTOR_POWER_BOOST_TIME                              43
-#define ADDRESS_STARTUP_MOTOR_POWER_BOOST_FADE_TIME                         44
-#define ADDRESS_MOTOR_TEMPERATURE_MIN_VALUE_LIMIT                           45
-#define ADDRESS_MOTOR_TEMPERATURE_MAX_VALUE_LIMIT                           46
-#define ADDRESS_BATTERY_VOLTAGE_RESET_WH_COUNTER_X10_0                      47
-#define ADDRESS_BATTERY_VOLTAGE_RESET_WH_COUNTER_X10_1                      48
-#define ADDRESS_LCD_POWER_OFF_TIME                                          49
-#define ADDRESS_LCD_BACKLIGHT_ON_BRIGHTNESS                                 50
-#define ADDRESS_LCD_BACKLIGHT_OFF_BRIGHTNESS                                51
-#define ADDRESS_BATTERY_PACK_RESISTANCE_0                                   52
-#define ADDRESS_BATTERY_PACK_RESISTANCE_1                                   53
-#define ADDRESS_OFFROAD_FEATURE_ENABLED                                     54
-#define ADDRESS_OFFROAD_MODE_ENABLED_ON_STARTUP                             55
-#define ADDRESS_OFFROAD_SPEED_LIMIT                                         56
-#define ADDRESS_OFFROAD_POWER_LIMIT_ENABLED                                 57
-#define ADDRESS_OFFROAD_POWER_LIMIT_DIV25                                   58
-#define ADDRESS_ODOMETER_X10_0                                              59
-#define ADDRESS_ODOMETER_X10_1                                              60
-#define ADDRESS_ODOMETER_X10_2                                              61
-#define ADDRESS_WALK_ASSIST_FEATURE_ENABLED                                 62
-#define ADDRESS_WALK_ASSIST_LEVEL_FACTOR_1                                  63
-#define ADDRESS_WALK_ASSIST_LEVEL_FACTOR_2                                  64
-#define ADDRESS_WALK_ASSIST_LEVEL_FACTOR_3                                  65
-#define ADDRESS_WALK_ASSIST_LEVEL_FACTOR_4                                  66
-#define ADDRESS_WALK_ASSIST_LEVEL_FACTOR_5                                  67
-#define ADDRESS_WALK_ASSIST_LEVEL_FACTOR_6                                  68
-#define ADDRESS_WALK_ASSIST_LEVEL_FACTOR_7                                  69
-#define ADDRESS_WALK_ASSIST_LEVEL_FACTOR_8                                  70
-#define ADDRESS_WALK_ASSIST_LEVEL_FACTOR_9                                  71
-#define EEPROM_BYTES_STORED                                                 72 // CAN'T BE HIGHER THAN 255
+typedef struct eeprom_data
+{
+  uint8_t ui8_assist_level;
+  uint16_t ui16_wheel_perimeter;
+  uint8_t ui8_wheel_max_speed;
+  uint8_t ui8_units_type;
+  uint32_t ui32_wh_x10_offset;
+  uint32_t ui32_wh_x10_100_percent;
+  uint8_t ui8_battery_soc_enable;
+  uint8_t ui8_battery_max_current;
+  uint8_t ui8_battery_cells_number;
+  uint16_t ui16_battery_low_voltage_cut_off_x10;
+  uint8_t ui8_motor_type;
+  uint8_t ui8_motor_assistance_startup_without_pedal_rotation;
+  uint8_t ui8_temperature_limit_feature_enabled;
+  uint8_t ui8_temperature_field_config;
+  uint8_t ui8_assist_level_factor[9];
+  uint8_t ui8_number_of_assist_levels;
+  uint8_t ui8_startup_motor_power_boost_feature_enabled;
+  uint8_t ui8_startup_motor_power_boost_state;
+  uint8_t ui8_startup_motor_power_boost_factor[9];
+  uint8_t ui8_startup_motor_power_boost_time;
+  uint8_t ui8_startup_motor_power_boost_fade_time;
+  uint8_t ui8_motor_temperature_min_value_to_limit;
+  uint8_t ui8_motor_temperature_max_value_to_limit;
+  uint16_t ui16_battery_voltage_reset_wh_counter_x10;
+  uint8_t ui8_lcd_power_off_time_minutes;
+  uint8_t ui8_lcd_backlight_on_brightness;
+  uint8_t ui8_lcd_backlight_off_brightness;
+  uint16_t ui16_battery_pack_resistance_x1000;
+  uint8_t ui8_offroad_feature_enabled;
+  uint8_t ui8_offroad_enabled_on_startup;
+  uint8_t ui8_offroad_speed_limit;
+  uint8_t ui8_offroad_power_limit_enabled;
+  uint8_t ui8_offroad_power_limit_div25;
+  uint32_t ui32_odometer_x10;
+  uint8_t ui8_walk_assist_feature_enabled;
+  uint8_t ui8_walk_assist_level_factor[9];
+  lcd_configurations_menu_t lcd_configurations_menu;
+} eeprom_data_t;
 
 void eeprom_init(void);
 void eeprom_init_variables(void);
 void eeprom_write_variables(void);
-void eeprom_erase_key_value(void);
+void eeprom_init_defaults(void);
 
 #endif /* _EEPROM_H_ */
--- Bafang_color_LCD_850C/Bafang_LCD_850C_firmware/src/lcd.c
+++ Bafang_color_LCD_850C/Bafang_LCD_850C_firmware/src/lcd.c
@@ -42,9 +42,9 @@ lcd_vars_t m_lcd_vars =
 };
 
 volatile l2_vars_t l2_vars;
-volatile l3_vars_t l3_vars;
+l3_vars_t l3_vars;
 
-static struct_lcd_configurations_vars *p_lcd_configurations_vars;
+static lcd_configurations_menu_t *p_lcd_configurations_vars;
 
 static uint8_t ui8_lcd_menu_counter_100ms_state = 0;
 
@@ -108,10 +108,10 @@ void lcd_init(void)
   UG_FillScreen(C_BLACK);
 
   lcd_configurations_screen_init();
-  p_lcd_configurations_vars = get_lcd_configurations_vars();
+  p_lcd_configurations_vars = get_lcd_configurations_menu();
 
   // init variables with the stored value on EEPROM
-  eeprom_init_variables ();
+  eeprom_init_variables();
 
   m_p_graphs = get_graphs();
 }
@@ -132,8 +132,8 @@ void lcd_clock(void)
     ui32_g_layer_2_can_execute = 1;
   }
 
-  if(first_time_management())
-    return;
+//  if(first_time_management())
+//    return;
 
   update_menu_flashing_state();
 
@@ -143,7 +143,7 @@ void lcd_clock(void)
   if(buttons_get_up_down_click_event() &&
       m_lcd_vars.lcd_screen_state == LCD_SCREEN_MAIN)
   {
-    buttons_clear_up_down_click_event();
+    buttons_clear_all_events();
 
     // reset needed variables of configurations screen
     p_lcd_configurations_vars->ui8_refresh_full_menu_1 = 1;
@@ -549,12 +549,13 @@ void assist_level_state(void)
   if (buttons_get_up_click_event() &&
       m_lcd_vars.ui8_lcd_menu_max_power == 0)
   {
-    buttons_clear_up_click_event ();
-    buttons_clear_up_click_long_click_event ();
-    buttons_clear_up_long_click_event ();
-    buttons_clear_down_click_event ();
-    buttons_clear_down_click_long_click_event ();
-    buttons_clear_down_long_click_event ();
+//    buttons_clear_up_click_event ();
+//    buttons_clear_up_click_long_click_event ();
+//    buttons_clear_up_long_click_event ();
+//    buttons_clear_down_click_event ();
+//    buttons_clear_down_click_long_click_event ();
+//    buttons_clear_down_long_click_event ();
+      buttons_clear_all_events();
 
     l3_vars.ui8_assist_level++;
 
@@ -565,12 +566,13 @@ void assist_level_state(void)
   if (buttons_get_down_click_event() &&
       m_lcd_vars.ui8_lcd_menu_max_power == 0)
   {
-    buttons_clear_up_click_event ();
-    buttons_clear_up_click_long_click_event ();
-    buttons_clear_up_long_click_event ();
-    buttons_clear_down_click_event ();
-    buttons_clear_down_click_long_click_event ();
-    buttons_clear_down_long_click_event ();
+//    buttons_clear_up_click_event ();
+//    buttons_clear_up_click_long_click_event ();
+//    buttons_clear_up_long_click_event ();
+//    buttons_clear_down_click_event ();
+//    buttons_clear_down_click_long_click_event ();
+//    buttons_clear_down_long_click_event ();
+      buttons_clear_all_events();
 
     if (l3_vars.ui8_assist_level > 0)
       l3_vars.ui8_assist_level--;
@@ -589,7 +591,7 @@ void assist_level_state(void)
   }
 }
 
-volatile l3_vars_t* get_l3_vars(void)
+l3_vars_t* get_l3_vars(void)
 {
   return &l3_vars;
 }
@@ -1150,7 +1152,7 @@ void lights_state(void)
   uint32_t ui32_x2;
   uint32_t ui32_y2;
 
-  if (buttons_get_up_long_click_event())
+  if(buttons_get_up_long_click_event())
   {
     buttons_clear_up_long_click_event();
 

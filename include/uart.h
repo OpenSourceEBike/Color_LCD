a1f7f1e3acb172d057616ac72eb566b2c3cac458
Kevin Hester <kevinh@geeksville.com>
2019-08-14 09:34:02 -0700

    oops - didn't mean to checkin

--- include/uart.h
+++ include/uart.h
@@ -10,7 +10,4 @@ void uart_send_tx_buffer(uint8_t* tx_buffer);
 #define UART_NUMBER_DATA_BYTES_TO_SEND      6   // change this value depending on how many data bytes there is to send ( Package = one start byte + data bytes + two bytes 16 bit CRC )
 #define UART_MAX_NUMBER_MESSAGE_ID          8   // change this value depending on how many different packages there is to send
 
-// Public only for debugging/fault messages
-extern uint8_t uart_rx_state_machine;
-
 #endif /* INCLUDE_UART_H_ */

b5a09d814388cbaf9502581cad96e4e872e83b22
lowPerformer <49353266+lowPerformer@users.noreply.github.com>
2019-08-15 16:55:33 +0200

    Implementation of uart RX ringbuffer

--- include/uart.h
+++ include/uart.h
@@ -9,6 +9,8 @@ void uart_send_tx_buffer(uint8_t* tx_buffer);
 #define UART_NUMBER_DATA_BYTES_TO_RECEIVE   25  // change this value depending on how many data bytes there is to receive ( Package = one start byte + data bytes + two bytes 16 bit CRC )
 #define UART_NUMBER_DATA_BYTES_TO_SEND      6   // change this value depending on how many data bytes there is to send ( Package = one start byte + data bytes + two bytes 16 bit CRC )
 #define UART_MAX_NUMBER_MESSAGE_ID          8   // change this value depending on how many different packages there is to send
+#define UART_NUMBER_CRC_BYTES               2
+#define UART_NUMBER_START_BYTES             1
 
 // Public only for debugging/fault messages
 extern uint8_t uart_rx_state_machine;
--- src/common/uart.c
+++ src/common/uart.c
@@ -10,11 +10,19 @@
 #include "uart.h"
 #include "utils.h"
 #include "assert.h"
+#include "app_util_platform.h"
 
 nrf_drv_uart_t uart0 = NRF_DRV_UART_INSTANCE(UART0);
-uint8_t uart_buffer0_rx[UART_NUMBER_DATA_BYTES_TO_RECEIVE + 3];
-uint8_t uart_buffer0_tx[11];
-volatile bool uart_rx_new_package;
+typedef struct uart_rx_buff_typedef uart_rx_buff_typedef;
+struct uart_rx_buff_typedef
+{
+  uint8_t uart_rx_data[UART_NUMBER_START_BYTES + UART_NUMBER_DATA_BYTES_TO_RECEIVE + UART_NUMBER_CRC_BYTES];
+  uart_rx_buff_typedef* next_uart_rx_buff;
+};
+uart_rx_buff_typedef* uart_rx_buffer;
+volatile uint8_t* uart_rx_data_rdy;
+
+uint8_t uart_buffer0_tx[UART_NUMBER_START_BYTES + UART_NUMBER_DATA_BYTES_TO_SEND + UART_NUMBER_CRC_BYTES];
 
 /* Function prototype */
 static void uart_event_handler(nrf_drv_uart_event_t *p_event, void *p_context);
@@ -24,6 +32,12 @@ static void uart_event_handler(nrf_drv_uart_event_t *p_event, void *p_context);
  */
 void uart_init(void)
 {
+  /* Init RX buffer */
+  static uart_rx_buff_typedef rxb1, rxb2;
+  rxb1.next_uart_rx_buff = &rxb2;
+  rxb2.next_uart_rx_buff = &rxb1;
+  uart_rx_buffer = &rxb1;
+
   /* Init driver */
   nrf_drv_uart_config_t uart_config = NRF_DRV_UART_DEFAULT_CONFIG;
   uart_config.pselrxd = UART_RX__PIN;
@@ -32,7 +46,7 @@ void uart_init(void)
   APP_ERROR_CHECK(nrf_drv_uart_init(&uart0, &uart_config, uart_event_handler));
   /* Enable & start RX (bytewise scanning for start byte) */
   nrf_drv_uart_rx_enable(&uart0);
-  APP_ERROR_CHECK(nrf_drv_uart_rx(&uart0, &uart_buffer0_rx[0], 1));
+  APP_ERROR_CHECK(nrf_drv_uart_rx(&uart0, &uart_rx_buffer->uart_rx_data[0], 1));
 }
 
 /**
@@ -40,13 +54,18 @@ void uart_init(void)
  */
 const uint8_t* uart_get_rx_buffer_rdy(void)
 {
-  if (uart_rx_new_package)
-  {
-    uart_rx_new_package = false;  // pull only once
-    return uart_buffer0_rx;
-  }
+  uint8_t* rx_rdy;
 
-  return NULL;
+  // VERY paranoid but it is possible that uart_rx_data_rdy
+  // is set from IRQ during hand-over
+  CRITICAL_REGION_ENTER();
+  {
+    rx_rdy = (uint8_t*) uart_rx_data_rdy;
+    uart_rx_data_rdy = NULL;
+  }
+  CRITICAL_REGION_EXIT();
+
+  return rx_rdy;
 }
 
 /**
@@ -63,7 +82,7 @@ uint8_t* uart_get_tx_buffer(void)
 void uart_send_tx_buffer(uint8_t *tx_buffer)
 {
   ret_code_t err_code = nrf_drv_uart_tx(&uart0, tx_buffer,
-  UART_NUMBER_DATA_BYTES_TO_SEND + 3);
+  UART_NUMBER_START_BYTES + UART_NUMBER_DATA_BYTES_TO_SEND + UART_NUMBER_CRC_BYTES);
 
   APP_ERROR_CHECK(err_code);
 }
@@ -85,7 +104,7 @@ static void uart_event_handler(nrf_drv_uart_event_t *p_event, void *p_context)
     // assert(p_event->data.error.error_mask & (UART_ERRORSRC_OVERRUN_Msk | UART_ERRORSRC_FRAMING_Msk | UART_ERRORSRC_BREAK_Msk));
 
     uart_rx_state_machine = 0;
-    APP_ERROR_CHECK(nrf_drv_uart_rx(&uart0, &uart_buffer0_rx[0], 1));
+    APP_ERROR_CHECK(nrf_drv_uart_rx(&uart0, &uart_rx_buffer->uart_rx_data[0], 1));
     break;
 
   case NRF_DRV_UART_EVT_RX_DONE:
@@ -93,48 +112,26 @@ static void uart_event_handler(nrf_drv_uart_event_t *p_event, void *p_context)
     {
     /* End of bytewise RX */
     case 0:
-      uart_rx_new_package = false;  // RX ongoing. Invalidate flag.
-      if (uart_buffer0_rx[0] == 0x43) // see if we get start package byte
+      if (uart_rx_buffer->uart_rx_data[0] == 0x43) // see if we get start package byte
       {
         uart_rx_state_machine = 1;
-        APP_ERROR_CHECK(nrf_drv_uart_rx(&uart0, &uart_buffer0_rx[1],
-            UART_NUMBER_DATA_BYTES_TO_RECEIVE + 2)); // Start RX of the remaining stream at once
+        APP_ERROR_CHECK(nrf_drv_uart_rx(&uart0, &uart_rx_buffer->uart_rx_data[1],
+            UART_NUMBER_DATA_BYTES_TO_RECEIVE + UART_NUMBER_CRC_BYTES)); // Start RX of the remaining stream at once
       }
       else
-        APP_ERROR_CHECK(nrf_drv_uart_rx(&uart0, &uart_buffer0_rx[0], 1)); // Next bytewise RX to check for start byte
+        APP_ERROR_CHECK(nrf_drv_uart_rx(&uart0, &uart_rx_buffer->uart_rx_data[0], 1)); // Next bytewise RX to check for start byte
       break;
 
     /* End of stream RX */
-    case 1: {
-      // validation of the package data
-      // last byte is the checksum
-      uint16_t ui16_crc_rx = 0xffff;
-      for (uint8_t ui8_i = 0; ui8_i <= UART_NUMBER_DATA_BYTES_TO_RECEIVE;
-          ui8_i++)
-      {
-        crc16(uart_buffer0_rx[ui8_i], &ui16_crc_rx);
-      }
-
-      if (((((uint16_t) uart_buffer0_rx[UART_NUMBER_DATA_BYTES_TO_RECEIVE + 2])
-          << 8)
-          + ((uint16_t) uart_buffer0_rx[UART_NUMBER_DATA_BYTES_TO_RECEIVE + 1]))
-          == ui16_crc_rx)
-        // {
-        // copy to the other buffer only if we processed already the last package
-        // FIXME - this flipflop between buffers can be done with just changing a pointer, no need for an expensive memcpy
-        // if(!ui8_received_package_flag)
-        //{
-        uart_rx_new_package = true; // signal that we have a full package to be processed
-
-      // store the received data to rx_buffer
-      // memcpy(ui8_rx_buffer, ui8_rx, UART_NUMBER_DATA_BYTES_TO_RECEIVE + 1);
-      //}
-
-      /* Start bytewise RX again (regardless of if we liked the current packet or not) */
+    case 1:
+      /* Signal that we have a full package to be processed */
+      uart_rx_data_rdy = uart_rx_buffer->uart_rx_data;
+      /* Switch buffer */
+      uart_rx_buffer = uart_rx_buffer->next_uart_rx_buff;
+      /* Start bytewise RX again */
       uart_rx_state_machine = 0;
-      APP_ERROR_CHECK(nrf_drv_uart_rx(&uart0, &uart_buffer0_rx[0], 1));
-    }
-    break;
+      APP_ERROR_CHECK(nrf_drv_uart_rx(&uart0, &uart_rx_buffer->uart_rx_data[0], 1));
+      break;
 
     default:
       assert(0);

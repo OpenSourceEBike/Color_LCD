1ca236624e1c48496df04e01598ddf74d5ef6c15
Kevin Hester <kevinh@geeksville.com>
2019-08-29 14:50:59 -0700

    all of the state update for mainscreen should be shared code IMO

--- firmware/common/include/mainscreen.h
+++ firmware/common/include/mainscreen.h
@@ -4,6 +4,28 @@
 
 void mainscreen_show();
 void main_idle(); // call every 20ms
+bool mainscreen_onpress(buttons_events_t events);
 
 extern Screen mainScreen, infoScreen, bootScreen;
+
+extern Screen *screens[];
+
+extern Field socField,
+	timeField,
+	assistLevelField,
+	wheelSpeedIntegerField,
+	wheelSpeedDecimalField,
+	maxPowerField,
+	humanPowerField,
+	warnField,
+	tripTimeField,
+	tripDistanceField,
+	odoField,
+	motorTempField,
+	pwmDutyField,
+	motorErpsField,
+	motorFOCField,
+	cadenceField;
+
+extern Field batteryField; // These fields are custom for board type
 

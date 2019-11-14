b55f6bae096e2429256bfee5d1f4d78baf16dff2
Kevin Hester <kevinh@geeksville.com>
2019-11-14 11:58:30 -0800

    Remove copypasta in config menu
    @casainho, I think I've properly backmerged the small number of diffs.
    I wanted to do this before things diverge too much.  cool?
    
    The theory being there should be very few differences between 850 and sw102
    config (other than shorter strings for the small display)

--- firmware/common/src/configscreen.c
+++ firmware/common/src/configscreen.c
@@ -3,7 +3,6 @@
 #include "configscreen.h"
 #include "eeprom.h"
 
-#ifdef SW102
 static Field wheelMenus[] =
 		{
 						FIELD_EDITABLE_UINT("Max speed", &l3_vars.wheel_max_speed_x10, "kph", 1, 990, .div_digits = 1, .inc_step = 10, .hide_fraction = true),
@@ -24,9 +23,9 @@ static Field batterySOCMenus[] =
 		{
 						FIELD_EDITABLE_ENUM("Feature", &l3_vars.ui8_battery_soc_enable, "disable", "enable"),
 						FIELD_EDITABLE_ENUM("Show", &l3_vars.ui8_battery_soc_increment_decrement, "% full", "% used"),
-						FIELD_EDITABLE_UINT("Reset at", &l3_vars.ui16_battery_voltage_reset_wh_counter_x10, "volts", 160, 630, .div_digits = 1),
-						FIELD_EDITABLE_UINT("Battery total", &l3_vars.ui32_wh_x10_100_percent, "whr", 0, 9990, .inc_step = 10),
-						FIELD_EDITABLE_UINT("Used", &l3_vars.ui32_wh_x10_offset, "whr", 0, 99900, .div_digits = 1, .inc_step = 100),
+						FIELD_EDITABLE_UINT(_S("Reset at voltage", "Reset at"), &l3_vars.ui16_battery_voltage_reset_wh_counter_x10, "volts", 160, 630, .div_digits = 1),
+						FIELD_EDITABLE_UINT(_S("Battery total Wh", "Battery total"), &l3_vars.ui32_wh_x10_100_percent, "whr", 0, 9990, .inc_step = 10),
+						FIELD_EDITABLE_UINT("Used Wh", &l3_vars.ui32_wh_x10_offset, "whr", 0, 99900, .div_digits = 1, .inc_step = 100),
 				FIELD_END };
 
 static Field assistMenus[] =
@@ -77,16 +76,23 @@ static Field startupPowerMenus[] =
 
 static Field motorTempMenus[] =
 		{
-						FIELD_EDITABLE_ENUM("Feature", &l3_vars.ui8_temperature_limit_feature_enabled, "disable", "enable"), // FIXME, share one array of disable/enable strings
+						FIELD_EDITABLE_ENUM("Feature", &l3_vars.ui8_temperature_limit_feature_enabled, "disable", "temperature", "throttle"), // FIXME, share one array of disable/enable strings
 						FIELD_EDITABLE_UINT("Min limit", &l3_vars.ui8_motor_temperature_min_value_to_limit, "degC", 0, 125),
 						FIELD_EDITABLE_UINT("Max limit", &l3_vars.ui8_motor_temperature_max_value_to_limit, "degC", 0, 125),
 				FIELD_END };
 
 static Field displayMenus[] =
 		{
-						FIELD_EDITABLE_UINT("Auto poweroff", &l3_vars.ui8_lcd_power_off_time_minutes, "mins", 0, 255),
-				// FIELD_EDITABLE_UINT("Reset to defaults", &ui8_reset_to_defaults_counter, "", 0, 255), // FIXME, make sure if the user incs to 10 we are doing the reset
-						FIELD_EDITABLE_ENUM("Units", &l3_vars.ui8_units_type, "SI", "Imperial"),
+#ifndef SW102
+		    FIELD_EDITABLE_UINT("Clock hours", &ui8_g_configuration_clock_hours, "", 0, 23, .onPreSetEditable = onSetConfigurationClockHours),
+		    FIELD_EDITABLE_UINT("Clock minutes", &ui8_g_configuration_clock_minutes, "", 0, 59, .onPreSetEditable = onSetConfigurationClockMinutes),
+        FIELD_EDITABLE_UINT("Brightness on", &l3_vars.ui8_lcd_backlight_on_brightness, "", 5, 100, .inc_step = 5, .onPreSetEditable = onSetConfigurationDisplayLcdBacklightOnBrightness),
+        FIELD_EDITABLE_UINT("Brightness off", &l3_vars.ui8_lcd_backlight_off_brightness, "", 5, 100, .inc_step = 5, .onPreSetEditable = onSetConfigurationDisplayLcdBacklightOffBrightness),
+        FIELD_EDITABLE_ENUM("Buttons invert", &l3_vars.ui8_buttons_up_down_invert, "default", "invert"),
+#endif
+		    FIELD_EDITABLE_UINT("Auto power off", &l3_vars.ui8_lcd_power_off_time_minutes, "mins", 0, 255),
+		    FIELD_EDITABLE_ENUM("Reset to defaults", &ui8_g_configuration_display_reset_to_defaults, "no", "yes"),
+		    FIELD_EDITABLE_ENUM("Units", &l3_vars.ui8_units_type, "SI", "Imperial"),
 				FIELD_END };
 
 #if 0
@@ -108,15 +114,15 @@ static Field variousMenus[] =
 
 static Field technicalMenus[] =
 		{
-		FIELD_READONLY_UINT("ADC throttle", &l3_vars.ui8_adc_throttle, ""),
-		FIELD_READONLY_UINT("Throttle", &l3_vars.ui8_throttle, ""),
-						FIELD_READONLY_UINT("ADC Torque", &l3_vars.ui8_adc_pedal_torque_sensor, ""),
-						FIELD_READONLY_UINT("Torque", &l3_vars.ui8_pedal_torque_sensor, ""),
-						FIELD_READONLY_UINT("Cadence", &l3_vars.ui8_pedal_cadence, "rpm"),
-						FIELD_READONLY_UINT("Human power", &l3_vars.ui16_pedal_power_x10, "W", .div_digits = 1),
-						FIELD_READONLY_UINT("PWM duty cycle", &l3_vars.ui8_duty_cycle, ""),
-						FIELD_READONLY_UINT("Motor speed", &l3_vars.ui16_motor_speed_erps, ""),
-				FIELD_READONLY_UINT("Motor FOC", &l3_vars.ui8_foc_angle, ""),
+		    FIELD_READONLY_UINT("ADC throttle sensor", &l3_vars.ui8_adc_throttle, ""),
+		    FIELD_READONLY_UINT("Throttle sensor", &l3_vars.ui8_throttle, ""),
+		    FIELD_READONLY_UINT("ADC Torque sensor", &l3_vars.ui8_adc_pedal_torque_sensor, ""),
+		    FIELD_READONLY_UINT("Torque sensor", &l3_vars.ui8_pedal_torque_sensor, ""),
+		    FIELD_READONLY_UINT("Pedal cadence", &l3_vars.ui8_pedal_cadence, "rpm"),
+		    FIELD_READONLY_UINT("Pedal Human power", &l3_vars.ui16_pedal_power_x10, "W", .div_digits = 1),
+		    FIELD_READONLY_UINT("PWM duty-cycle", &l3_vars.ui8_duty_cycle, ""),
+		    FIELD_READONLY_UINT("Motor speed", &l3_vars.ui16_motor_speed_erps, ""),
+		    FIELD_READONLY_UINT("Motor FOC", &l3_vars.ui8_foc_angle, ""),
 				FIELD_END };
 
 static Field topMenus[] = {
@@ -125,140 +131,13 @@ FIELD_SCROLLABLE("Battery", batteryMenus),
 FIELD_SCROLLABLE(_S("Battery SOC", "Bat SOC"), batterySOCMenus),
 FIELD_SCROLLABLE(_S("Assist level", "Assist"), assistMenus),
 FIELD_SCROLLABLE("Walk", walkAssistMenus),
-FIELD_SCROLLABLE(_S("Startup power", "Start Pwr"), startupPowerMenus),
-FIELD_SCROLLABLE("Motor temp", motorTempMenus),
+FIELD_SCROLLABLE(_S("Startup BOOST", "Start Pwr"), startupPowerMenus),
+FIELD_SCROLLABLE(_S("Motor temperature", "Motor temp"), motorTempMenus),
 FIELD_SCROLLABLE("Display", displayMenus),
 // FIELD_SCROLLABLE("Offroad", offroadMenus),
 		FIELD_SCROLLABLE("Various", variousMenus),
 		FIELD_SCROLLABLE("Technical", technicalMenus),
 		FIELD_END };
-
-#else // 850C display
-
-static Field wheelMenus[] = {
-  FIELD_EDITABLE_UINT("Max speed", &l3_vars.wheel_max_speed_x10, "kph", 1, 990, .div_digits = 1, .inc_step = 10, .hide_fraction = true),
-  FIELD_EDITABLE_UINT("Circumference", &l3_vars.ui16_wheel_perimeter, "mm", 750, 3000, .inc_step = 10),
-  FIELD_END };
-
-static Field batteryMenus[] = {
-  FIELD_EDITABLE_UINT("Max current", &l3_vars.ui8_battery_max_current, "amps", 1, 30),
-  FIELD_EDITABLE_UINT("Current ramp", &l3_vars.ui8_ramp_up_amps_per_second_x10, "amps", 4, 255, .div_digits = 1),
-  FIELD_EDITABLE_UINT("Low cut-off voltage", &l3_vars.ui16_battery_low_voltage_cut_off_x10, "volts", 160, 630, .div_digits = 1),
-  FIELD_EDITABLE_UINT("Number cells", &l3_vars.ui8_battery_cells_number, "", 7, 14),
-  FIELD_EDITABLE_UINT("Resistance", &l3_vars.ui16_battery_pack_resistance_x1000, "mohm", 0, 1000),
-  FIELD_READONLY_UINT("Voltage (read only)", &l3_vars.ui16_battery_voltage_soc_x10, "volts", .div_digits = 1),
-  FIELD_END };
-
-static Field batterySOCMenus[] = {
-  FIELD_EDITABLE_ENUM("Feature", &l3_vars.ui8_battery_soc_enable, "disable", "enable"),
-  FIELD_EDITABLE_ENUM("Show", &l3_vars.ui8_battery_soc_increment_decrement, "% full", "% used"),
-  FIELD_EDITABLE_UINT("Reset at voltage", &l3_vars.ui16_battery_voltage_reset_wh_counter_x10, "volts", 160, 630, .div_digits = 1),
-  FIELD_EDITABLE_UINT("Battery total Wh", &ui32_g_configuration_wh_100_percent, "whr", 0, 9990, .inc_step = 10, .onPreSetEditable = onSetConfigurationBatteryTotalWh),
-  FIELD_EDITABLE_UINT("Used Wh", &l3_vars.ui32_wh_x10, "whr", 0, 99900, .div_digits = 1, .inc_step = 100),
-  FIELD_END };
-
-static Field assistMenus[] = {
-  FIELD_EDITABLE_UINT("Num assist levels", &l3_vars.ui8_number_of_assist_levels, "", 1, 9),
-  FIELD_EDITABLE_UINT("Level 1", &l3_vars.ui8_assist_level_factor[0], "", 0, 255, .div_digits = 1),
-  FIELD_EDITABLE_UINT("Level 2", &l3_vars.ui8_assist_level_factor[1], "", 0, 255, .div_digits = 1),
-  FIELD_EDITABLE_UINT("Level 3", &l3_vars.ui8_assist_level_factor[2], "", 0, 255, .div_digits = 1),
-  FIELD_EDITABLE_UINT("Level 4", &l3_vars.ui8_assist_level_factor[3], "", 0, 255, .div_digits = 1),
-  FIELD_EDITABLE_UINT("Level 5", &l3_vars.ui8_assist_level_factor[4], "", 0, 255, .div_digits = 1),
-  FIELD_EDITABLE_UINT("Level 6", &l3_vars.ui8_assist_level_factor[5], "", 0, 255, .div_digits = 1),
-  FIELD_EDITABLE_UINT("Level 7", &l3_vars.ui8_assist_level_factor[6], "", 0, 255, .div_digits = 1),
-  FIELD_EDITABLE_UINT("Level 8", &l3_vars.ui8_assist_level_factor[7], "", 0, 255, .div_digits = 1),
-  FIELD_EDITABLE_UINT("Level 9", &l3_vars.ui8_assist_level_factor[8], "", 0, 255, .div_digits = 1),
-  FIELD_END };
-
-static Field walkAssistMenus[] = {
-  FIELD_EDITABLE_ENUM("Feature", &l3_vars.ui8_walk_assist_feature_enabled, "disable", "enable"), // FIXME, share one array of disable/enable strings
-  FIELD_EDITABLE_UINT("Level 1", &l3_vars.ui8_walk_assist_level_factor[0], "", 0, 100),
-  FIELD_EDITABLE_UINT("Level 2", &l3_vars.ui8_walk_assist_level_factor[1], "", 0, 100),
-  FIELD_EDITABLE_UINT("Level 3", &l3_vars.ui8_walk_assist_level_factor[2], "", 0, 100),
-  FIELD_EDITABLE_UINT("Level 4", &l3_vars.ui8_walk_assist_level_factor[3], "", 0, 100),
-  FIELD_EDITABLE_UINT("Level 5", &l3_vars.ui8_walk_assist_level_factor[4], "", 0, 100),
-  FIELD_EDITABLE_UINT("Level 6", &l3_vars.ui8_walk_assist_level_factor[5], "", 0, 100),
-  FIELD_EDITABLE_UINT("Level 7", &l3_vars.ui8_walk_assist_level_factor[6], "", 0, 100),
-  FIELD_EDITABLE_UINT("Level 8", &l3_vars.ui8_walk_assist_level_factor[7], "", 0, 100),
-  FIELD_EDITABLE_UINT("Level 9", &l3_vars.ui8_walk_assist_level_factor[8], "", 0, 100),
-  FIELD_END };
-
-static Field startupPowerMenus[] = {
-  FIELD_EDITABLE_ENUM("Feature", &l3_vars.ui8_startup_motor_power_boost_feature_enabled, "disable", "enable"), // FIXME, share one array of disable/enable strings
-  FIELD_EDITABLE_ENUM("Active on", &l3_vars.ui8_startup_motor_power_boost_always, "startup", "always"),
-  FIELD_EDITABLE_ENUM("Limit to max-power", &l3_vars.ui8_startup_motor_power_boost_limit_power, "no", "yes"),
-  FIELD_EDITABLE_ENUM("Needs rotation", &l3_vars.ui8_motor_assistance_startup_without_pedal_rotation, "disable", "enable"), // FIXME, share one array of disable/enable strings
-  FIELD_EDITABLE_UINT("Duration", &l3_vars.ui8_startup_motor_power_boost_time, "secs", 0, 255, .div_digits = 1),
-  FIELD_EDITABLE_UINT("Fade", &l3_vars.ui8_startup_motor_power_boost_fade_time, "secs", 0, 255, .div_digits = 1),
-  FIELD_EDITABLE_UINT("Level 1", &l3_vars.ui8_startup_motor_power_boost_factor[0], "", 0, 255, .div_digits = 1),
-  FIELD_EDITABLE_UINT("Level 2", &l3_vars.ui8_startup_motor_power_boost_factor[1], "", 0, 255, .div_digits = 1),
-  FIELD_EDITABLE_UINT("Level 3", &l3_vars.ui8_startup_motor_power_boost_factor[2], "", 0, 255, .div_digits = 1),
-  FIELD_EDITABLE_UINT("Level 4", &l3_vars.ui8_startup_motor_power_boost_factor[3], "", 0, 255, .div_digits = 1),
-  FIELD_EDITABLE_UINT("Level 5", &l3_vars.ui8_startup_motor_power_boost_factor[4], "", 0, 255, .div_digits = 1),
-  FIELD_EDITABLE_UINT("Level 6", &l3_vars.ui8_startup_motor_power_boost_factor[5], "", 0, 255, .div_digits = 1),
-  FIELD_EDITABLE_UINT("Level 7", &l3_vars.ui8_startup_motor_power_boost_factor[6], "", 0, 255, .div_digits = 1),
-  FIELD_EDITABLE_UINT("Level 8", &l3_vars.ui8_startup_motor_power_boost_factor[7], "", 0, 255, .div_digits = 1),
-  FIELD_EDITABLE_UINT("Level 9", &l3_vars.ui8_startup_motor_power_boost_factor[8], "", 0, 255, .div_digits = 1),
-  FIELD_END };
-
-static Field motorTempMenus[] = {
-  FIELD_EDITABLE_ENUM("Feature", &l3_vars.ui8_temperature_limit_feature_enabled, "disable", "temperature", "throttle"), // FIXME, share one array of disable/enable strings
-  FIELD_EDITABLE_UINT("Min limit", &l3_vars.ui8_motor_temperature_min_value_to_limit, "degC", 0, 125),
-  FIELD_EDITABLE_UINT("Max limit", &l3_vars.ui8_motor_temperature_max_value_to_limit, "degC", 0, 125),
-  FIELD_END };
-
-static Field displayMenus[] = {
-  FIELD_EDITABLE_UINT("Clock hours", &ui8_g_configuration_clock_hours, "", 0, 23, .onPreSetEditable = onSetConfigurationClockHours),
-  FIELD_EDITABLE_UINT("Clock minutes", &ui8_g_configuration_clock_minutes, "", 0, 59, .onPreSetEditable = onSetConfigurationClockMinutes),
-  FIELD_EDITABLE_ENUM("Units", &l3_vars.ui8_units_type, "SI", "Imperial"),
-  FIELD_EDITABLE_ENUM("Buttons invert", &l3_vars.ui8_buttons_up_down_invert, "default", "invert"),
-  FIELD_EDITABLE_UINT("Brightness on", &l3_vars.ui8_lcd_backlight_on_brightness, "", 5, 100, .inc_step = 5, .onPreSetEditable = onSetConfigurationDisplayLcdBacklightOnBrightness),
-  FIELD_EDITABLE_UINT("Brightness off", &l3_vars.ui8_lcd_backlight_off_brightness, "", 5, 100, .inc_step = 5, .onPreSetEditable = onSetConfigurationDisplayLcdBacklightOffBrightness),
-  FIELD_EDITABLE_UINT("Auto power off", &l3_vars.ui8_lcd_power_off_time_minutes, "mins", 0, 255),
-  FIELD_EDITABLE_ENUM("Reset to defaults", &ui8_g_configuration_display_reset_to_defaults, "no", "yes"),
-  FIELD_END };
-
-#if 0
-static Field offroadMenus[] = {
-    FIELD_EDITABLE_ENUM("Feature", &l3_vars.ui8_offroad_feature_enabled, "disable", "enable"), // FIXME, share one array of disable/enable strings
-    FIELD_EDITABLE_ENUM("Active on start", &l3_vars.ui8_offroad_enabled_on_startup, "no", "yes"), // FIXME, share one array of disable/enable strings
-    FIELD_EDITABLE_UINT("Speed limit", &l3_vars.ui8_offroad_speed_limit_x10, "kph", 1, 990, .div_digits = 10, .inc_step = 10, .hide_fraction = true),
-    FIELD_EDITABLE_ENUM("Limit power", &l3_vars.ui8_offroad_power_limit_enabled, "no", "yes"), // FIXME, share one array of disable/enable strings
-    FIELD_EDITABLE_UINT("Power limit", &l3_vars.ui8_offroad_power_limit_div25, "watt", 0, 2000), // huge FIXME - div25 can't work with this system, change it
-    FIELD_END
-};
-#endif
-
-static Field variousMenus[] = {
-  FIELD_EDITABLE_ENUM("Motor voltage", &l3_vars.ui8_motor_type, "48V", "36V", "expert"),
-  FIELD_END };
-
-static Field technicalMenus[] = {
-  FIELD_READONLY_UINT("ADC throttle sensor", &l3_vars.ui8_adc_throttle, ""),
-  FIELD_READONLY_UINT("Throttle sensor", &l3_vars.ui8_throttle, ""),
-  FIELD_READONLY_UINT("ADC Torque sensor", &l3_vars.ui8_adc_pedal_torque_sensor, ""),
-  FIELD_READONLY_UINT("Torque sensor", &l3_vars.ui8_pedal_torque_sensor, ""),
-  FIELD_READONLY_UINT("Pedal cadence", &l3_vars.ui8_pedal_cadence, "rpm"),
-  FIELD_READONLY_UINT("Pedal Human power", &l3_vars.ui16_pedal_power_x10, "W", .div_digits = 1),
-  FIELD_READONLY_UINT("PWM duty-cycle", &l3_vars.ui8_duty_cycle, ""),
-  FIELD_READONLY_UINT("Motor speed", &l3_vars.ui16_motor_speed_erps, ""),
-  FIELD_READONLY_UINT("Motor FOC", &l3_vars.ui8_foc_angle, ""),
-  FIELD_END };
-
-static Field topMenus[] = {
-  FIELD_SCROLLABLE("Wheel", wheelMenus),
-  FIELD_SCROLLABLE("Battery", batteryMenus),
-  FIELD_SCROLLABLE("Battery SOC", batterySOCMenus),
-  FIELD_SCROLLABLE("Assist level", assistMenus),
-  FIELD_SCROLLABLE("Walk assist", walkAssistMenus),
-  FIELD_SCROLLABLE("Startup BOOST", startupPowerMenus),
-  FIELD_SCROLLABLE("Motor temperature", motorTempMenus),
-  FIELD_SCROLLABLE("Display", displayMenus),
-  // FIELD_SCROLLABLE("Offroad", offroadMenus),
-  FIELD_SCROLLABLE("Various", variousMenus),
-  FIELD_SCROLLABLE("Technical", technicalMenus),
-  FIELD_END };
-#endif
 
 static Field configRoot = FIELD_SCROLLABLE(_S("Configurations", "Config"), topMenus);
 

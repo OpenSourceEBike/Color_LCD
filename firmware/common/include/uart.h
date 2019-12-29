9ef76e3ca04d2628441335ec0a46ee93ff885757
Kevin Hester <kevinh@geeksville.com>
2019-12-29 15:18:33 -0800

    kinda fix build for:
    Compiling file: uart.c
    ./src/sw102/uart.c:19:50: error: 'UART_NUMBER_DATA_BYTES_TO_RECEIVE_V20' undeclared here (not in a function)
       uint8_t uart_rx_data[UART_NUMBER_START_BYTES + UART_NUMBER_DATA_BYTES_TO_RECEIVE_V20 + UART_NUMBER_CRC_BYTES];
                                                      ^
    ./src/sw102/uart.c:27:51: error: 'UART_NUMBER_DATA_BYTES_TO_SEND_V20' undeclared here (not in a function)
     uint8_t uart_buffer0_tx[UART_NUMBER_START_BYTES + UART_NUMBER_DATA_BYTES_TO_SEND_V20 + UART_NUMBER_CRC_BYTES];
                                                       ^
    ./src/sw102/uart.c: In function 'uart_init':
    ./src/sw102/uart.c:39:26: error: 'UART_NUMBER_DATA_BYTES_TO_RECEIVE_V19' undeclared (first use in this function)
       uart_number_bytes_rx = UART_NUMBER_DATA_BYTES_TO_RECEIVE_V19;
                              ^
    ./src/sw102/uart.c:39:26: note: each undeclared identifier is reported only once for each function it appears in
    ./src/sw102/uart.c:40:26: error: 'UART_NUMBER_DATA_BYTES_TO_SEND_V19' undeclared (first use in this function)
       uart_number_bytes_tx = UART_NUMBER_DATA_BYTES_TO_SEND_V19;
                              ^
    ./src/sw102/uart.c: In function 'uart_set_stream_version':
    ./src/sw102/uart.c:75:28: error: 'UART_NUMBER_DATA_BYTES_TO_RECEIVE_V19' undeclared (first use in this function)
         uart_number_bytes_rx = UART_NUMBER_DATA_BYTES_TO_RECEIVE_V19;
                                ^
    ./src/sw102/uart.c:76:28: error: 'UART_NUMBER_DATA_BYTES_TO_SEND_V19' undeclared (first use in this function)
         uart_number_bytes_tx = UART_NUMBER_DATA_BYTES_TO_SEND_V19;
                                ^

--- firmware/common/include/uart.h
+++ firmware/common/include/uart.h
@@ -9,8 +9,11 @@ const uint8_t* uart_get_rx_buffer_rdy(void);
 uint8_t* uart_get_tx_buffer(void);
 void uart_send_tx_buffer(uint8_t *tx_buffer);
 
-#define UART_NUMBER_DATA_BYTES_TO_RECEIVE       28
-#define UART_NUMBER_DATA_BYTES_TO_SEND          84
+#define UART_NUMBER_DATA_BYTES_TO_RECEIVE_V19       28
+#define UART_NUMBER_DATA_BYTES_TO_SEND_V19          84
+
+#define UART_NUMBER_DATA_BYTES_TO_RECEIVE_V20       28 // @lowPerformer please fix - @geeksville just made up these numbers
+#define UART_NUMBER_DATA_BYTES_TO_SEND_V20          84
 
 #define UART_NUMBER_CRC_BYTES                   2
 #define UART_NUMBER_START_BYTES                 1
